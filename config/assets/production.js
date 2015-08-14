bf7123b63f90dab9398b0a909dbbc022e5b46806
Sean Emmer <sean.emmer@gmail.com>
2015-08-14 12:53:12 -0700

    changed heroku config

--- config/assets/production.js
+++ config/assets/production.js
@@ -8,15 +8,15 @@ module.exports = {
         'public/lib/font-awesome/css/font-awesome.css'
       ],
       js: [
-        'public/lib/jquery/dist/jquery.js',
-        'public/lib/angular/angular.js',
-        'public/lib/lodash/lodash.js',
-        'public/lib/angular-animate/angular-animate.js',
-        'public/lib/angular-aria/angular-aria.js',
-        'public/lib/angular-material/angular-material.js',
-        'public/lib/angular-resource/angular-resource.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.js',
-        'public/lib/angular-ui-utils/ui-utils.js'
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/angular/angular.min.js',
+        'public/lib/lodash/lodash.min.js',
+        'public/lib/angular-animate/angular-animate.min.js',
+        'public/lib/angular-aria/angular-aria.min.js',
+        'public/lib/angular-material/angular-material.min.js',
+        'public/lib/angular-resource/angular-resource.min.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/angular-ui-utils/ui-utils.min.js'
       ],
     },
     css: 'public/dist/application.min.css',
